46baed3eda1d9a5ebd839a59a4c9e915fbb3445d
romanin1 <37311016+romanin1@users.noreply.github.com>
2019-09-10 16:46:10 -0400

    Update MLEkalmanDKunivariate.m

--- MLEkalmanDKunivariate.m
+++ MLEkalmanDKunivariate.m
@@ -35,40 +35,41 @@
 
 
 %==========================================================================
-function [MLEoutput, csi, CSI, P, p, f, eta] = MLEkalmanDKunivariate(y, X, H, csi_not, P_not, x)
+function [MLEoutput, csi, CSI, P, p, f, F, eta] = MLEkalmanDKunivariate(y, X, H, csi_not, P_not, x)
 
 
 % % ------------ MLE parameters ------------
 
 [T , M] = size(y); % number of time periods and variables in the dependent vector
 r = size(H, 2); % number of states
-k = size(X, 2); % number of observables
+obs = size(X, 2); % number of observables
 
 
 
 % M*k + r + M*M  % number of parameters to be estimated
-
-% x = [0.325827824675063,0.287027007587544,-0.00510762367536082,1,1,1,1,1,1,1,1,1,0.762498749538479,0.497746241891913,-0.103945958353170,0.497746241891913,69.2441126931429,-4.20414343841562,-0.103945958353170,-4.20414343841562,42.3910797499956];
+% y = Y;
+% X = X;
+% H = Hkal;
+% x = x0;
 
 % Setting matrices of parameters to be estimated. 
 % A contains observable coefficients
+% R the Mx1 diagonal varcovar matrix of the endogenous variables in the system
 % Q is the (time-fixed, for now) diagonal variance of states
-% R the MxM varcovar matrix of the endogenous variables in the system
 
 A = [];
-for i = 1:M*k
+for i = 1:M*obs
     A = [A', x(i)]';
 end
-A = reshape(A, M, k);
+A = reshape(A, M, obs);
 
 R = [];
-for i = M*k+1: M*k+(M*M)
-    R = [R', x(i)^2]';
+for i = M*obs+1: M*obs+M
+    R = [R, x(i)^2];
 end
-R = reshape(R, M, M);
 
 Q = [];
-for i = M*k+(M*M)+1: M*k+(M*M)+r
+for i = M*obs+M+1: M*obs+M+r
     Q = [Q', x(i)^2]';
 end
 Q = diag(Q);
@@ -113,14 +114,15 @@ for t = 1:T
             P{t, 1} = p{t};
         end
 
+
         % Filtering equations
-        eta(t,i) = y(t,i) - A*X(t,:) - H(t, :)* CSI{t, i}; % forecast error for the observation equation   
-        f(t,i) = H(t, :)*P{t, i}* H(t,:)' + R; % mean squared error (+ uncertainty)
-        k{t, i} = P{t, i} * H(t, :)' * pinv(f(t,i)); % Kalman Gain
+        eta(t,i) = y(t,i) - A(i)*X(t,:) - H(t+(i-1)*T, :)* CSI{t, i}; % forecast error for the observation equation   
+        f(t,i) = H( t+(i-1)*T, :)*P{t, i}* H( t+(i-1)*T,:)' + R(i); % mean squared error (+ uncertainty)
+        k{t, i} = P{t, i} * H(t+(i-1)*T, :)' * pinv(f(t,i)); % Kalman Gain
 
         % Update     
         CSI{t, i+1} = CSI{t, i} +  k{t, i} * eta(t,i);
-        P{t, i+1} = P{t, i} - k{t, i} * H(t, :) * P{t, i};
+        P{t, i+1} = P{t, i} - k{t, i} * H( t+(i-1)*T, :) * P{t, i};
 
         % Likelihood
         iterMLE = iterMLE + log(f(t,i)) + eta(t,i)'*pinv(f(t,i))*eta(t,i);
